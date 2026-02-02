7ddbf2a6cc8efb2740995fd65eaeb2c54828ce29
ariunzulm <munkhbatt.ariunzul@gmail.com>
2026-02-02 21:04:20 +0800

    hero

---
+++ src/app/homescreen/_components/Nativagation.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { ChevronDown, Search } from "lucide-react";
+
+export default function Navigation() {
+  return (
+    <div className="flex flex-col  max-w-360 mx-auto w-full bg-white">
+      <section className="w-full flex items-center justify-between py-3 px-5 md:px-12 xl:px-16 xl:gap-4">
+        <img src="/Logo.svg" alt="logo" className="xl:h-10" />
+
+        <div className="flex gap-3 md:hidden">
+          <img
+            src="/search-icon.svg"
+            alt="search icon"
+            className="cursor-pointer md:hidden"
+          />
+          <img src="/nightmode-icon.svg" alt="nightmode icon" />
+        </div>
+
+        <div className="hidden md:flex gap-3 xl:gap-4 items-center justify-center">
+          <Button
+            variant="link"
+            className="text-sm py-2 px-3 font-medium border border-zinc-200 hover:bg-gray-50 cursor-pointer"
+          >
+            <ChevronDown />
+            Genre
+          </Button>
+
+          <InputGroup className="min-w-94.75">
+            <InputGroupInput placeholder="Search..." />
+            <InputGroupAddon>
+              <Search />
+            </InputGroupAddon>
+          </InputGroup>
+
+          <img
+            src="/nightmode-icon.svg"
+            alt="nightmode icon"
+            className="cursor-pointer hover:opacity-70 transition-opacity"
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,109 @@
-import Image from "next/image";
+import { Star } from "lucide-react";
+import Navigation from "./homescreen/_components/Nativagation";
+import { Button } from "@/components/ui/button";
+import { Play } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="min-h-screen bg-gray-50">
+      <Navigation />
+      <HeroSection />
     </div>
   );
 }
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col max-w-360 mx-auto w-full bg-white">
+      <div className="md:hidden">
+        <div className="relative w-full">
+          <img
+            src="/scroll-1.jpg"
+            alt="wicked poster"
+            className="w-full h-auto object-cover"
+          />
+          <div className="absolute bottom-4 left-1/2 -traslate-x-1/2 flex gap-2">
+            <div className="w-2 h-2 rounded-full bg-white" />
+            <div className="w-2 h-2 rounded-full bg-white" />
+            <div className="w-2 h-2 rounded-full bg-white" />
+          </div>
+        </div>
+      </div>
+
+      <section className="px-5 py-3 space-y-4 flex justify-between">
+        <div className="flex flex-col items-start gap-4">
+          <div>
+            <p className="text-sm leading-5 text-gray-500">Now Playing:</p>
+            <h3 className="text-2xl font-semibold text-gray-900">Wicked</h3>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-1.5 px-3 py-1.5">
+          <Star fill="#FDE047" className="text-amber-200" />
+          <p className="text-lg font-semibold text-gray-900">6.9</p>3
+          <p className="text-base text-gray-400">/10</p>
+        </div>
+      </section>
+      <p className="text-sm text-gray-900 px-5 leading-relaxed">
+        Elphaba, a misunderstood young woman because of her green skin, and
+        Glinda, a popular girl, become friends at Shiz University in the Land of
+        Oz. After an encounter with the Wonderful Wizard of Oz, their friendship
+        reaches a crossroads.
+      </p>
+
+      <section className="px-5 py-3 ">
+        <Button
+          variant="link"
+          className="bg-black text-white text-sm py-2 px-3 w-fit font-semibold"
+        >
+          <Play />
+          Watch Trailer
+        </Button>
+      </section>
+
+      <section className="hidden md:block relative w-full">
+        <div className="relative w-full h-125 lg:h-150 xl:h-170">
+          <img
+            src="/scroll-1.jpg"
+            alt="wicked poster"
+            className="w-full h-auto object-cover"
+          />
+          <div className="absolute inset-0 flex flex-col items-start">
+            <div className="max-w-360 text-white mx-auto w-full px-8 md:px-12 xl:px-16">
+              <p className="text-sm lg:text-base xl:text-lg font-medium leading-relaxed">
+                Now Playing:
+              </p>
+              <h3 className="text-4xl lg:text-6xl xl:text-7xl font-semibold  leading-tight">
+                Wicked
+              </h3>
+            </div>
+
+            <div className="flex items-center gap-2">
+              <Star fill="#FDE047" className="text-amber-200" />
+              <p className="text-2xl lg:text-3xl xl:text-4xl font-semibold">
+                6.9
+              </p>
+              3
+              <p className="text-lg lg:text-xl xl:text-xl text-gray-300">/10</p>
+            </div>
+            <p className="text-sm lg:text-base xl:text-lg text-white leading-relaxed">
+              Elphaba, a misunderstood young woman because of her green skin,
+              and Glinda, a popular girl, become friends at Shiz University in
+              the Land of Oz. After an encounter with the Wonderful Wizard of
+              Oz, their friendship reaches a crossroads.
+            </p>
+            <section className="px-5 py-3 ">
+              <Button
+                variant="link"
+                className="bg-black text-white text-sm py-2 px-3 w-fit font-semibold"
+              >
+                <Play />
+                Watch Trailer
+              </Button>
+            </section>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+/* <UpcomingCarousel />*/
